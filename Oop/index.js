8ca0abcaff70d8e8da5f1b8fe740da24622deeeb
Mert Aksoy <mrtaksy99@gmail.com>
2024-06-27 18:26:49 +0300

    Oop with TypeScript
    Calculator App with TypeScript

---
+++ Oop/index.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");//görüntüyü alma
+const result = 0;
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+//Eval kodda içine ne yazarsan onu çalıştırır.Burada Display de her ne yazıyorsa o işlemi yapacak
+//Ancak büyük bir güvenlik açığı oluşuturuyor kullanmamak en iyisi.
+//hata ve güvenlik için try catch
+function calculate(){
+  try{
+    display.value = eval(display.value); 
+  }
+  catch(error){
+    display.value="Error";
+  }
+}
